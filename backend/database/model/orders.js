6ad8182311bf56c425c7f20482db88669e4994d1
Fyun <fei.yun.unhi@gmail.com>
2020-03-12 12:18:29 -0400

    add backend

---
+++ backend/database/model/orders.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+//define collections
+
+let Order=new Schema({
+    order_id:{type:String},
+    customer_id:{type:String},
+    subtotal:{type:Number},
+    tax:{type:Number},
+    item_id:{type:String},
+    quantity:{type:Number},
+    comment:{type:String},
+    total:{type:String},
+    date:{type:Date},
+    collection: 'orders'   
+})
