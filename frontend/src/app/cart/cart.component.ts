4fae27f3bee546015352b9b309490cbe225cf486
Ihor Bodnarchuk <Ihor.Bodnarchuk@fsresidential.com>
2020-04-09 17:15:27 -0400

    fix calculation logic

--- frontend/src/app/cart/cart.component.ts
+++ frontend/src/app/cart/cart.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CartService } from '../cart.service';
-import{OrderService} from '../order.service';
-import { FormGroup, FormBuilder, FormControl, FormArray} from '@angular/forms';
-import {Router} from '@angular/router';
-import {Order, orderProducts} from '../Order';
-import { empty } from 'rxjs';
-import { summaryFileName } from '@angular/compiler/src/aot/util';
+import { OrderService } from '../order.service';
+import { FormGroup, FormBuilder, FormControl, FormArray, AbstractControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
 interface Province {
   value: string;
   viewValue: string;
@@ -21,97 +20,110 @@ interface paymentMethod {
   styleUrls: ['./cart.component.css']
 })
 
-
-
-export class CartComponent implements OnInit {
-   angForm: FormGroup;
-   subtotal:number;
-  constructor(private fb: FormBuilder,private cartService: CartService,private orderService:OrderService , private router: Router
-    ) {this.createForm();
-    }
+export class CartComponent implements OnInit, OnDestroy {
+  subscriptions: Subscription[] = [];
+  angForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private cartService: CartService, 
+    private orderService: OrderService, 
+    private router: Router) {}
   //display part
-  
+
   //interface of province option
   provinces: Province[] = [
-    {value: 'ON', viewValue: 'Ontario'},
-    {value: 'AB', viewValue: 'Alberta'},
-    {value: 'BC', viewValue: 'British Columbia'},
-    {value: 'QC', viewValue: 'Quebec'}
+    { value: 'ON', viewValue: 'Ontario' },
+    { value: 'AB', viewValue: 'Alberta' },
+    { value: 'BC', viewValue: 'British Columbia' },
+    { value: 'QC', viewValue: 'Quebec' }
   ];
-//interface of payment option
+  //interface of payment option
   payments: paymentMethod[] = [
-    {value: 'Visa'},
-    {value: 'MasterCard'},
-    {value: 'American Express'}
+    { value: 'Visa' },
+    { value: 'MasterCard' },
+    { value: 'American Express' }
   ];
   // object for display items in cart
   items;
 
   //object for add to database
-  order;
+  order = [];
 
   //object for collect forms
-  createForm(){
-  this.angForm=this.fb.group({
-    status:[''],
-    subtotal:[],
-    tax:[],
-    total:[],
-    date:[],
-    orderProducts:this.fb.array([]),
-    paymentMethod:[''],
-    customerFullName:[''],
-    phone:[''],
-    email:[''],
-    shippingAddress:[''],
-    deliveryNote:[''],
-    city:[''],
-    province:[''],
-    postalCode:[''],
-  })
-}
-
-
-get ProductOrder(){
-  return this.angForm.controls['orderProducts'] as FormArray;
-}
-
-  addOrder(subtotal,tax,total,orderProducts,paymentMethod,customerFullName,phone,email,shippingAddress,deliveryNote,city,province,postalCode){
-   this.orderService.addOrder('TobeConfirmed',subtotal,tax,total,new Date(),orderProducts.value,paymentMethod,customerFullName,phone,email,shippingAddress,deliveryNote,city,province,postalCode,'');
-    setTimeout(()=>this.router.navigate(['success']),900);
+  createForm() {
+    this.angForm = this.fb.group({
+      status: [''],
+      subtotal: 0,
+      tax: [],
+      total: [],
+      date: [],
+      orderProducts: this.fb.array([]),
+      paymentMethod: [''],
+      customerFullName: [''],
+      phone: [''],
+      email: [''],
+      shippingAddress: [''],
+      deliveryNote: [''],
+      city: [''],
+      province: [''],
+      postalCode: [''],
+    })
   }
-  orders=[];
+
+  get ProductOrder() {
+    return this.angForm.controls['orderProducts'] as FormArray;
+  }
+
+  addOrder(subtotal, tax, total, orderProducts, paymentMethod, customerFullName, phone, email, shippingAddress, deliveryNote, city, province, postalCode) {
+    this.orderService.addOrder('TobeConfirmed', subtotal, tax, total, new Date(), orderProducts.value, paymentMethod, customerFullName, phone, email, shippingAddress, deliveryNote, city, province, postalCode, '');
+    setTimeout(() => this.router.navigate(['success']), 900);
+  }
+
+  updateCartSubtotal(): void {
+    const forms = this.ProductOrder.controls as FormGroup[];
+    const newSubTotal = forms.map(x => +x.get('subbyitem').value)
+                             .reduce((acc, val) => acc + val);
+    this.angForm.controls.subtotal.setValue(newSubTotal);
+  }
 
   ngOnInit() {
+    this.createForm();
+  
     this.items = this.cartService.getItems();
 
-    
-    for(let i of this.items){
+    for (let i of this.items) {
       this.ProductOrder.push(new FormGroup({
-      productName:new FormControl(i.name),
-      isDonation:new FormControl(false),
-      isGift:new FormControl(false),
-      comment:new FormControl(null),
-      price:new FormControl(i.price),
-      quantity:new FormControl(0),
-      subbyitem:new FormControl(0),
-      }),)
- }
-
- var qtyFormControls = this.ProductOrder.controls.map(x => x.get('quantity'));
-
-   
-
-    this.ProductOrder.valueChanges.subscribe(value => {
-      var qtyFormControls = this.ProductOrder.controls.map(x => x.get('quantity'));
-      console.log(qtyFormControls)
-
-      
-   })
-   console.log(this.subtotal);
-
+        productName: new FormControl(i.name),
+        isDonation: new FormControl(false),
+        isGift: new FormControl(false),
+        comment: new FormControl(null),
+        price: new FormControl(i.price),
+        quantity: new FormControl(0),
+        subbyitem: new FormControl(0),
+      }))
     }
-   
-  
+
+    const orderProducts: FormArray = this.angForm.get('orderProducts') as FormArray;
+    const forms: FormGroup[] = orderProducts.controls as FormGroup[];
+    const qtyFormControls: AbstractControl[] = forms.map(x => x.get('quantity'));
+
+    qtyFormControls.forEach(qfc => {
+      const parent: FormGroup = qfc.parent as FormGroup;
+      const subscription = qfc.valueChanges.subscribe(qty => {
+        const price: number = parent.get('price').value;
+        parent.get('subbyitem').setValue(qty * price)
+        this.updateCartSubtotal();
+      });
+
+     this.subscriptions.push(subscription);
+    });
+
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s.unsubscribe());
+  }
 
 }
+
+
